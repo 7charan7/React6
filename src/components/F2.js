6b1bf377cfd6bbc9c04131028f9402c45011c09f
7charan7 <charanadimalla7@gmail.com>
2023-10-15 18:41:30 +0530

    first commit

---
+++ src/components/F2.js
@@ -0,0 +1,369 @@
+import "./style.css";
+export function Home() {
+    return (
+        <div>
+            <header id="header">
+                <div class="container">
+
+                    <h1><i>Charan Adimalla</i></h1>
+                    <h2>I'm a <span>Developer</span> </h2>
+
+
+                    <div class="social-links">
+                        <a href="https://www.linkedin.com/in/charan-adimalla-80b269296/" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                        <a href="https://github.com/7charan7" class="github"><i class="bi bi-github"></i></a>
+                    </div>
+
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export function About() {
+    return (
+        <div>
+            <div class="about-me container">
+
+                <div class="section-title">
+                    <h2>About</h2>
+                    <p>Learn more about me</p>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-4" data-aos="fade-right">
+                        <img src="assets/img/me.jpg" class="img-fluid" alt="" />
+                    </div>
+                    <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+                        <h3>Charan Adimalla</h3>
+                        <p class="fst-italic">
+                            3rd B.tech Graduate from VIT-AP
+                        </p>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <ul>
+                                    <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>10 Oct 2003</span></li>
+                                    <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+91 9652974481</span></li>
+                                    <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Vijayawada, India</span></li>
+                                </ul>
+                            </div>
+                            <div class="col-lg-6">
+                                <ul>
+                                    <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>20</span></li>
+                                    <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>UnderGraduate</span></li>
+                                    <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong>
+                                        <span>charanadimalla7@gmail.com</span></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <p>
+                            I am a core student at VIT-AP University, passionate about using technology to solve real-world problems. I
+                            am an excellent communicator and collaborator.
+                            I am committed to excellence and staying up-to-date on the latest tech trends. I believe that collaboration
+                            is essential for success, and I am eager to contribute to the tech industry in a meaningful way.
+                        </p>
+                    </div>
+                    <div class="skills container">
+
+                        <div class="section-title">
+                            <h2>Skills</h2>
+                        </div>
+
+                        <div class="row skills-content">
+
+                            <div class="col-lg-6">
+
+                                <div class="progress">
+                                    <span class="skill">JAVA <i class="val">80%</i></span>
+                                    <div class="progress-bar-wrap">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="progress">
+                                    <span class="skill">PYTHON <i class="val">80%</i></span>
+                                    <div class="progress-bar-wrap">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="progress">
+                                    <span class="skill">MySQL <i class="val">65%</i></span>
+                                    <div class="progress-bar-wrap">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="progress">
+                                    <span class="skill">R-Programming <i class="val">60%</i></span>
+                                    <div class="progress-bar-wrap">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="progress">
+                                    <span class="skill">MATLab <i class="val">70%</i></span>
+                                    <div class="progress-bar-wrap">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div class="col-lg-6">
+
+                                <div class="progress">
+                                    <span class="skill">HTML <i class="val">100%</i></span>
+                                    <div class="progress-bar-wrap">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="progress">
+                                    <span class="skill">CSS <i class="val">90%</i></span>
+                                    <div class="progress-bar-wrap">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="progress">
+                                    <span class="skill">JavaScript <i class="val">75%</i></span>
+                                    <div class="progress-bar-wrap">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="progress">
+                                    <span class="skill">REACT <i class="val">80%</i></span>
+                                    <div class="progress-bar-wrap">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="progress">
+                                    <span class="skill">Spring Boot <i class="val">70%</i></span>
+                                    <div class="progress-bar-wrap">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+                                        </div>
+                                    </div>
+                                </div>
+
+
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+            <div class="interests container">
+
+                <div class="section-title">
+                    <h2>Courses</h2>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 col-md-4">
+                        <div class="icon-box">
+                            <i class="ri-store-line" style={{ color: "#ffbb2c" }}></i>
+                            <h3>
+                                Object Oriented Programming
+                            </h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
+                        <div class="icon-box">
+                            <i class="ri-bar-chart-box-line" style={{ color: "#5578ff" }}></i>
+                            <h3>Data Structures and Algorithms</h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
+                        <div class="icon-box">
+                            <i class="ri-calendar-todo-line" style={{ color: "#e80368" }}></i>
+                            <h3>Operating Systems</h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-4 mt-4 mt-lg-0">
+                        <div class="icon-box">
+                            <i class="ri-paint-brush-line" style={{ color: "#e361ff" }}></i>
+                            <h3>Database Managemen Systems</h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-4 mt-4">
+                        <div class="icon-box">
+                            <i class="ri-database-2-line" style={{ color: "#47aeff" }}></i>
+                            <h3>Cloud Computing
+                            </h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-4 mt-4">
+                        <div class="icon-box">
+                            <i class="ri-gradienter-line" style={{ color: "#ffa76e" }}></i>
+                            <h3>Computer Networks
+                            </h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-4 mt-4">
+                        <div class="icon-box">
+                            <i class="ri-file-list-3-line" style={{ color: "#11dbcf" }}></i>
+                            <h3>Computer Organizationand Architecture
+                            </h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-4 mt-4">
+                        <div class="icon-box">
+                            <i class="ri-price-tag-2-line" style={{ color: "#4233ff" }}></i>
+                            <h3>Digital Image Processing
+                            </h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-4 mt-4">
+                        <div class="icon-box">
+                            <i class="ri-anchor-line" style={{ color: "#b2904f" }}></i>
+                            <h3>Software Engineering
+                            </h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-4 mt-4">
+                        <div class="icon-box">
+                            <i class="ri-disc-line" style={{ color: "#b20969" }}></i>
+                            <h3>Artificial Intelligence</h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-4 mt-4">
+                        <div class="icon-box">
+                            <i class="ri-base-station-line" style={{ color: "#ff5828" }}></i>
+                            <h3>Data Analytics</h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-4 mt-4">
+                        <div class="icon-box">
+                            <i class="ri-fingerprint-line" style={{ color: "#29cc61" }}></i>
+                            <h3>Web Development</h3>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export function Education() {
+    return (
+        <div>
+            <section id="Education" class="Education">
+                <div class="container">
+
+                    <div class="section-title">
+                        <h2>Education</h2>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-6">
+
+
+                            <h3 class="Education-title">Education</h3>
+                            <div class="Education-item">
+                                <h4>Intermediate </h4>
+                                <p><em>Narayana Educational Institutions, Vijayawada, AP</em></p>
+                                <h5>2019 - 2021</h5>
+                                <p><em>MARKS secured : 970 </em></p>
+                            </div>
+                            <div class="Education-item">
+                                <h4>Secondary Education
+                                </h4>
+                                <p><em>V.P.Siddhartha Public School, Vijayawada, AP</em></p>
+                                <h5>2018 - 2019</h5>
+                                <p><em>GPA secured : 10.0 </em></p>
+
+
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <h3 class="Education-title">Bachelor of Technology</h3>
+                            <div class="Education-item">
+                                <h4>
+
+                                    Vellore Institute of Technology
+
+                                </h4>
+                                <h5>2021 - Present</h5>
+                                <p><em>GPA secured : 8.64 </em></p>
+                                <p>
+                                    <ul>
+                                        <li>Technie
+                                        </li>
+                                        <li>Participated in many Technical Events
+                                        </li>
+                                    </ul>
+                                </p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export function Projects() {
+    return (
+        <div>
+            <section id="Projects" class="Projects">
+                <div class="container">
+
+                    <div class="section-title">
+                        <h2>Projects</h2>
+                        <p>My Projects</p>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+                            <div class="icon-box">
+                                <div class="icon"><i class="bx bxl-dribbble"></i></div>
+                                <h4><a href="">Toxic Comment Classifier</a></h4>
+                                <p>Toxic Comment Classifier using CNN</p>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+                            <div class="icon-box">
+                                <div class="icon"><i class="bx bx-tachometer"></i></div>
+                                <h4><a href="">Automatic Water Planting System</a></h4>
+                                <p>Automatic Water Planting System using Aurdino UNO</p>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+                            <div class="icon-box">
+                                <div class="icon"><i class="bx bx-world"></i></div>
+                                <h4><a href="">Hotstar Clone</a></h4>
+                                <p>Cloning Hotstar using HTML, CSS and Bootstrap</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </section>
+        </div>
+    )
+}
+
+
+
